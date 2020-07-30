69278933acd64d96bd73e9a8132d5197fa82fee2
asianmod <anthonynakamoto@gmail.com>
2020-07-30 00:14:26 -0300

    Só horizontais

--- cubo/config/routes.rb
+++ cubo/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    root to: 'home#welcome' 
+    resources :tasks
+#root define a rota '/' da nossa aplicação
 end
